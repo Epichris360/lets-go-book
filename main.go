637b0b1af7d7e82cbb5c218cf171f0014e5f2634
cristian <hellstorm12@gmail.com>
2019-12-29 15:01:04 -0300

    first chapter

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+//  Home func... corresponds to /
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippet box"))
+}
+
+// Shows a specific snippet
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet..."))
+}
+
+// creates a new snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
