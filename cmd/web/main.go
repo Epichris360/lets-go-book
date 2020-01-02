073658fb54f990e6b78f0d587dcbed2a727a20fb
cristian <hellstorm12@gmail.com>
2020-01-02 20:09:45 -0300

    custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 // page 82
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
 	flag.Parse()
+
+	// creates a new logger. standard out, extra text, additional info
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -21,7 +26,14 @@ func main() {
 	// use the mux.Handle() function to register the file server as the handler
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on port%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// initialize new http.Server struct to carry the error log func, port and mux
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on port%s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
