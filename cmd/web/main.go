808e03a3786c7dfd91f1d05cb9a8b561e3ecff2a
cristian <hellstorm12@gmail.com>
2020-01-02 08:06:16 -0300

    finished 2.9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// create a file server which serves files out of the './ui/static'
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// use the mux.Handle() function to register the file server as the handler
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
